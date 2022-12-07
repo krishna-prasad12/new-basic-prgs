3674616b2640c3ce434884bae4a428e7a0da0011
EscanoR <krichuzz@gmail.com>
2022-12-07 11:09:35 +0530

    next hangman commit

--- hangman.py
+++ hangman.py
@@ -9,11 +9,13 @@ lens=len(rands)
 for len in range(lens):
     emp.append('_')   # print lines for each of aphabet of random word
 print(emp)
-
-user_input=input('guess a alphabet of the word').lower()
-for rand in rands:
-    if rand==user_input:
-        new = rands.index(rand)
-        emp[new]=user_input
-print(new)
+while '_'  in emp:
+    user_input=input('guess a alphabet of the word').lower()
+    for rand in rands:
+        if rand==user_input:
+            new = rands.index(rand)
+            print(new)
+            emp[new]=user_input
+# print(new)
 print(emp)
+
