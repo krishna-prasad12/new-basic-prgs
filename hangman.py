4c6c1d11e3a50c256bfb61152fad99e3b68e8e84
EscanoR <krichuzz@gmail.com>
2022-12-07 10:29:16 +0530

    next hangman commit

--- hangman.py
+++ hangman.py
@@ -6,13 +6,14 @@ rands=random.choice(words)
 emp=[]
 print(f'correct word is {rands}')
 lens=len(rands)
-emp.append('_'*lens)   # print lines for each of aphabet of random word
+for len in range(lens):
+    emp.append('_')   # print lines for each of aphabet of random word
 print(emp)
 
 user_input=input('guess a alphabet of the word').lower()
 for rand in rands:
-    new=int(rands.index(rand))
-    print(new)
     if rand==user_input:
-        emp[0][new]=user_input
-
+        new = rands.index(rand)
+        emp[new]=user_input
+print(new)
+print(emp)
