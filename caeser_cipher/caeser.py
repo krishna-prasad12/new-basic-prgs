2a15e6eff22ac9929abbc193459bc5739c7ec29a
EscanoR <krichuzz@gmail.com>
2022-12-08 09:49:26 +0530

    caeser cipher

--- caeser_cipher/caeser.py
+++ caeser_cipher/caeser.py
@@ -3,26 +3,28 @@ alphabet = list(string.ascii_lowercase)
 new_alpha=list(string.ascii_lowercase)
 alphabet=alphabet+new_alpha
 print(alphabet)
+run_again='yes'
+while run_again=='yes':
+    choice=input('type 1.encode 2.decode of your text\t')
+    tex=input('input text u want to perform the action on \t').lower()
+    shift=int(input('input the shift to be used\t'))
 
-choice=input('type 1.encode 2.decode of your text\t')
-tex=input('input text u want to perform the action on \t').lower()
-shift=int(input('input the shift to be used\t'))
 
-
-def encode_text(tex,shift,choice):
-    cipher_text = ''
-    if choice=='encode':
-        shift*=-1
-    elif choice=='decode':
-        shift*=1
-    for letter in tex:
-        leter_pos=tex.index(letter)
-        pos=alphabet.index(letter)
-        shifts=pos+shift
-        new_letter=alphabet[shifts]
-        cipher_text+=new_letter
-    print( f"encoded text is :{cipher_text}")
-encode_text(tex,shift,choice)
+    def encode_text(tex,shift,choice):
+        cipher_text = ''
+        if choice=='encode':
+            shift*=-1
+        elif choice=='decode':
+            shift*=1
+        for letter in tex:
+            leter_pos=tex.index(letter)
+            pos=alphabet.index(letter)
+            shifts=pos+shift
+            new_letter=alphabet[shifts]
+            cipher_text+=new_letter
+        print( f"encoded text is :{cipher_text}")
+    encode_text(tex,shift,choice)
+    run_again=input('want to run agian (yes/no)')
 
 # def encode_text(tex,shift,choice):
 #     cipher_text = ''
