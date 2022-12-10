85469403df57178e715a9b595e35f248e99aa058
EscanoR <krichuzz@gmail.com>
2022-12-10 15:52:40 +0530

    guess no game

---
+++ higher or lower/data.py
@@ -0,0 +1,39 @@
+
+
+data=[{
+      'name':'Leo Messi',
+      'country':'Argentina',
+      'description':'Footballer',
+      'followers':386
+      },
+      {
+      'name':'Neymar JR',
+      'country':'Brazil',
+      'description':'Footballer',
+      'followers': 190
+      },
+      {
+      'name':'Mohanlal',
+      'country':'India',
+      'description':'Actor',
+      'followers': 4.9
+      },
+     {
+      'name':'Mammooty',
+      'country':'India',
+      'description':'Actor',
+      'followers': 3.4
+      },
+      {
+      'name':'Virat Kohli',
+      'country':'India',
+      'description':'Cricketer',
+      'followers':226
+      },
+      {
+      'name':'Rohit Sharma',
+      'country':'India',
+      'description':'Cricketer',
+      'followers': 26
+      }
+]
